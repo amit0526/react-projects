c10fb5ac3f031ceeb12e0851e63c51dcbcf50722
Amit Anand <amit.anand0526@gmail.com>
2026-08-08 20:50:47 +0530

    Complete input project

--- name-input/src/App.jsx
+++ name-input/src/App.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+
+  const [name, setname] = useState("");
+
   return (
     <>
-      <div className="flex bg-linear-[25deg,red_5%,yellow_60%,lime_90%,teal] min-h-screen items-center justify-center">
-        <div className="flex w-full max-w-sm min-h-70 p-3  justify-center items-center bg-gray-300 flex-col gap-5 rounded-3xl">
-          <div className="bg-amber-200 w-full rounded-xl -mt-10">
-            <h1 className="bg-rose-100 m-2 mtpx-2 text-center text-2xl font-bold rounded-2xl ">
-              Name-Input
+      <div className="min-h-screen w-full flex justify-center items-center bg-gray-100">
+        <div className="bg-gray-300 p-6 rounded-3xl w-full max-w-sm">
+            <h1 className="bg-linear-to-r from-sky-300 to-yellow-300 p-3 text-center text-2xl font-bold rounded-2xl">
+              Name Input
             </h1>
-          </div>
+
           <div>
-            <input className="bg-lime-200 rounded mt-7  p-2"></input>
+            <input className="bg-lime-200 rounded mt-7 p-2 outline-none"
+              type="text"
+              value={name}
+              onChange={(e) => setname(e.target.value)}
+              placeholder="Enter your input.." />
+            
+            <h2 className="text-4xl font-bold mt-5 text-center">Hello {name}</h2>
           </div>
+
         </div>
       </div>
     </>
   );
-}
+};
 
-export default App
+export default App;
