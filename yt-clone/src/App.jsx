142a5ca6f4a33ce2c8371122780965c06de95a60
Amit Anand <amit.anand0526@gmail.com>
2026-08-25 21:34:01 +0530

    Add tailwindcss in yt-clone app and remove app.css

--- yt-clone/src/App.jsx
+++ yt-clone/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>hello yt</h1>
+      <h1 className='bg-red-300 m-4 p-4 text-2xl text-center font-bold'>hello yt</h1>
     </div>
   )
 }
